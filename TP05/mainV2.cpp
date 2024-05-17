6ab1ebc165904bebaeae818ef0153d6bd11054f8
luchoSollima <165165923+luchoSollima@users.noreply.github.com>
2024-05-17 12:47:31 -0300

    Update mainV2.cpp

--- TP05/mainV2.cpp
+++ TP05/mainV2.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 int main()
 {
+	// Initialization of all variables.
+
 	const int arrayLimit = 8;
 	int maxCellsPerRow = 15;
 	int firstRow[arrayLimit] = { };
@@ -19,7 +21,11 @@ int main()
 	int printCounter = 0;
 	int arrayIndexCounter = 0;
 	bool maxPatternNumberReached = false;
+	bool maxPatternNumberExceeded = false;
 	bool firstPrint = true;
+	
+
+	// Welcome screen and explanation.
 
 	cout << "Bienvenido al creador de tablero del Nonograma!" << endl;
 	cout << "\nEste programa arma un tablero de Nonograma a partir del ingreso de los patrones de las filas. Los valores validos para los numeros del patron son de 0-15." << endl << endl;
@@ -27,22 +33,36 @@ int main()
 	system("pause");
 	system("cls");
 
+	// User input request.
+
 	cout << "Ingrese el patron para la primera fila:" << endl;
 	cout << "Ingrese un numero del patron (ingrese 0 si desea dejar el resto de la fila vacia)." << endl;
 
+	// Loop that fills firstRow array with the pattern for that row.
+
 	do
 	{
+		// Asks the user if they want to continue adding numbers to the pattern if there's still space. Doesn't print on the first iteration.
+
 		if (!firstPrint)
 			cout << "Todavia queda espacio en la fila. Quiere ingresar otro numero o pasar de fila (ingrese 0 para pasar de fila)?" << endl;
 
 		cin >> userInput;
 
+		// Adds userInput to the array.
+
 		firstRow[arrayIndexCounter] = userInput;
+
+		// Counts the total amount of blank and black cells.
 
 		if (firstRow[arrayIndexCounter] != 0)
 		{
 
+			// Adds the black cells.
+
 			totalCellNumber += firstRow[arrayIndexCounter];
+
+			// Adds the blank cells (one per group of black cells), except if the total amount of cells has reached the maximum per row.
 
 			if (totalCellNumber != maxCellsPerRow)
 			{
@@ -50,25 +70,43 @@ int main()
 			}
 		}
 
+		// Changes bool value to interrupt the firstRow input loop. This happens if the total amount of cells has reached the maximum.
+
 		if (totalCellNumber == maxCellsPerRow)
 			maxPatternNumberReached = true;
 
+		// If the user enters a number that exceeds the maxmimum number of cells per row, that number is removed from the array and the error message appears, prompting the user to enter another number.
+
 		if (totalCellNumber > maxCellsPerRow && userInput != 0)
 		{
+			// Removes the exceeded number value from the array.
+
 			totalCellNumber -= firstRow[arrayIndexCounter] + 1;
-			arrayIndexCounter--;
+			
+			// Changes bool value so that at the end of the loop, 1 is not added to arrayIndexCounter. This way, on the next iteration, the userInput will be assigned to the same elemente as the exceeded number (now removed),
+
+			maxPatternNumberExceeded = true;
 			cout << "Cantidad incorrecta de casillas. Ingrese nuevamente el patron:" << endl;
 		}
 
-		arrayIndexCounter++;
+		// Adds 1 to the arrayIndexCounter so that on the next iteration, the userInput will be assigned to the next element in the array (unless the last userInput value exceeded the maximum amount of cells per row).
+
+		if (!maxPatternNumberExceeded)
+			arrayIndexCounter++;
+
 		firstPrint = false;
 
 	} while (!maxPatternNumberReached && userInput != 0);
 
+	// Resets these variables.
+
 	arrayIndexCounter = 0;
 	totalCellNumber = 0;
+	maxPatternNumberExceeded = false;
 	maxPatternNumberReached = false;
 	firstPrint = true;
+
+	// Clears screen before moving on to the next row input.
 
 	system("cls");
 
@@ -249,28 +287,39 @@ int main()
 
 
 
-	// Print.
+	// Prints the array on screen.
 
 	for (int i = 0; i < arrayLimit; i++)
 	{
+		// Prints only one 0 if that's the first element. This is in case the user only entered the number 0 (empty row).
+
 		if (firstRow[0] == 0)
 		{
 			cout << firstRow[i] << " ";
 			break;
 		}
 		
+		// Prints the array ignoring elements that have a 0 value.
+
 		else if (firstRow[i] != 0)
 			cout << firstRow[i] << " ";
 	}
+
+	// Prints the cell pattern on screen.
 
 	for (int i = 0; i < arrayLimit; i++)
 	{
 		if (firstRow[i] != 0)
 		{
 
+			// Prints black cells as many times as the value on the element in the array (ignoring array elements that are 0).
+
 			for (int j = 0; j < firstRow[i]; j++)
 			{
 				cout << blackCell;
+
+				// Adds 1 to the printCounter, so that by the end of the printing loop the program will know how many cells were printed.
+
 				printCounter++;
 			}
 			if (printCounter != maxCellsPerRow)
@@ -282,11 +331,15 @@ int main()
 		}
 	}
 
+	// Prints blank cells if there are remaining unassigned cells.
+
 	while (printCounter != maxCellsPerRow)
 	{
 		cout << blankCell;
 		printCounter++;
 	}
+
+	// Resets printCounter.
 
 	printCounter = 0;
 	cout << endl;
