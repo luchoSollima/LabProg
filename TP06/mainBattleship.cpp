3ca5e1782313bdc04dcc7b6d964ecc82aee8c6b8
luchoSollima <165165923+luchoSollima@users.noreply.github.com>
2024-05-31 10:56:25 -0300

    Update mainBattleship.cpp

--- TP06/mainBattleship.cpp
+++ TP06/mainBattleship.cpp
@@ -49,29 +49,82 @@ void main()
 void CpuBattleshipGridGeneration()
 {
 	srand(time(0));
+	int randomRowCell = 0;
+	int randomColCell = 0;
+
 	int shipPlacementProbability = 0;
+	int verticalOrHorizontalPlacementProbability = 0;
+	bool shipVerticalPlacement = false;
+	bool shipHorizontalPlacement = false;
 	bool twoCellShipPlaced = false;
 	bool threeCellShipPlaced = false;
 	bool fourCellShipPlaced = false;
 	bool fiveCellShipPlaced = false;
 
-	for (int i = 0; i < maxRows; i++)
+	do
 	{
-		for (int j = 0; j < maxCols; j++)
+		randomRowCell = rand() % 10;
+		randomColCell = rand() % 10;
+
+		if (cpuBattleshipGrid[randomRowCell][randomColCell + 1] == 0 && cpuBattleshipGrid[randomRowCell + 1][randomColCell] == 0)
 		{
-			shipPlacementProbability = rand() % 2;
-			if (shipPlacementProbability == 1)
+			verticalOrHorizontalPlacementProbability = rand() % 2;
+
+			if (verticalOrHorizontalPlacementProbability == 0)
+				shipHorizontalPlacement = true;
+
+			else
+				shipVerticalPlacement = true;
+		}
+
+
+		/*for (int i = 0; i < maxRows; i++)
+		{
+			randomVerticalCell = rand() % 10;
+			randomHorizontalCell = rand() % 10;
+			for (int j = 0; j < maxCols; j++)
 			{
-				if ((cpuBattleshipGrid[i][j + 1] == 0 || cpuBattleshipGrid[i][j + 1] < maxCols - 1) && (cpuBattleshipGrid[i][j + 2] == 0 || cpuBattleshipGrid[i][j + 2] < maxCols - 1) && (cpuBattleshipGrid[i][j + 3] == 0 || cpuBattleshipGrid[i][j + 3] < maxCols - 1) && (cpuBattleshipGrid[i][j + 4] == 0 || cpuBattleshipGrid[i][j + 4] < maxCols - 1))
+				shipPlacementProbability = rand() % 2;
+				if (shipPlacementProbability == 1)
 				{
-					cpuBattleshipGrid[i][j] = 4;
-					cpuBattleshipGrid[i][j + 1] = 4;
-					cpuBattleshipGrid[i][j + 2] = 4;
-					cpuBattleshipGrid[i][j + 3] = 4;
+					if (!fiveCellShipPlaced && (cpuBattleshipGrid[i][j + 1] == 0 || cpuBattleshipGrid[i][j + 1] < maxCols - 1) && (cpuBattleshipGrid[i][j + 2] == 0 || cpuBattleshipGrid[i][j + 2] < maxCols - 1) && (cpuBattleshipGrid[i][j + 3] == 0 || cpuBattleshipGrid[i][j + 3] < maxCols - 1) && (cpuBattleshipGrid[i][j + 4] == 0 || cpuBattleshipGrid[i][j + 4] < maxCols - 1))
+					{
+						cpuBattleshipGrid[i][j] = 5;
+						cpuBattleshipGrid[i][j + 1] = 5;
+						cpuBattleshipGrid[i][j + 2] = 5;
+						cpuBattleshipGrid[i][j + 3] = 5;
+						cpuBattleshipGrid[i][j + 4] = 5;
+						fiveCellShipPlaced = true;
+					}
+
+					else if (!fourCellShipPlaced && (cpuBattleshipGrid[i][j + 1] == 0 || cpuBattleshipGrid[i][j + 1] < maxCols - 1) && (cpuBattleshipGrid[i][j + 2] == 0 || cpuBattleshipGrid[i][j + 2] < maxCols - 1) && (cpuBattleshipGrid[i][j + 3] == 0 || cpuBattleshipGrid[i][j + 3] < maxCols - 1))
+					{
+						cpuBattleshipGrid[i][j] = 4;
+						cpuBattleshipGrid[i][j + 1] = 4;
+						cpuBattleshipGrid[i][j + 2] = 4;
+						cpuBattleshipGrid[i][j + 3] = 4;
+						fourCellShipPlaced = true;
+					}
+
+					else if (!threeCellShipPlaced && (cpuBattleshipGrid[i][j + 1] == 0 || cpuBattleshipGrid[i][j + 1] < maxCols - 1) && (cpuBattleshipGrid[i][j + 2] == 0 || cpuBattleshipGrid[i][j + 2] < maxCols - 1))
+					{
+						cpuBattleshipGrid[i][j] = 3;
+						cpuBattleshipGrid[i][j + 1] = 3;
+						cpuBattleshipGrid[i][j + 2] = 3;
+						threeCellShipPlaced = true;
+					}
+
+					else if (!twoCellShipPlaced && (cpuBattleshipGrid[i][j + 1] == 0 || cpuBattleshipGrid[i][j + 1] < maxCols - 1))
+					{
+						cpuBattleshipGrid[i][j] = 2;
+						cpuBattleshipGrid[i][j + 1] = 2;
+						twoCellShipPlaced = true;
+					}
 				}
 			}
-		}
-	}
+		}*/
+
+	} while (true);
 }
 
 void CpuBattleshipGridPrint()
